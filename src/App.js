30c75d6aa5cfd622729689db705977bdf5dbafc3
sergiobni <bni.sergio@gmail.com>
2024-02-16 16:13:44 +0100

    Loading from fake API, handling load, error and ready status

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import DateCounter from './DateCounter';
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+
+const initialState = {
+  questions: [],
+
+  //Loading, error, ready, active, finished
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    default:
+      throw new Error('Action unknown');
+  }
+}
 
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  //Calculating derived state, the amount of questions
+  const numQuestions = questions.length;
+  //Load data onmount
+  useEffect(() => {
+    fetch(`http://localhost:8000/questions`)
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'dataReceived', payload: data }))
+      .catch(err => dispatch({ type: 'dataFailed' }));
+  });
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+      </Main>
     </div>
   );
 }
